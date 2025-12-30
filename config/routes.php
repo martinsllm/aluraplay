43e1c8c5a83a366ac65f50b0e24c71bd7995f568
martinsllm <lucaslm2010@gmail.com>
2025-12-30 19:30:09 -0300

    Fixing removal route url

--- config/routes.php
+++ config/routes.php
@@ -6,5 +6,5 @@ return [
     'POST|/novo-video' => Alura\Mvc\Controller\NewVideoController::class,
     'GET|/editar-video' => Alura\Mvc\Controller\FormController::class,
     'POST|/editar-video' => Alura\Mvc\Controller\UpdateVideoController::class,
-    'GET|/deletar-video' => Alura\Mvc\Controller\DeleteVideoController::class,
+    'GET|/remover-video' => Alura\Mvc\Controller\DeleteVideoController::class,
 ];
